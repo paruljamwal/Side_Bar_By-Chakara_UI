902909097d8228129bbdf75b28807d1772c31e3c
paruljamwal <paruljamwal2000@gmail.com>
2022-05-29 14:19:45 +0530

    submission

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,46 +1,64 @@
 import React from "react";
 import { Side } from "./Sidebar.style";
-import {AddIcon,SettingsIcon} from '@chakra-ui/icons'
-import { Button, ButtonGroup } from "@chakra-ui/react";
-import home from '../assets/home.png'
-import ex from '../assets/explore.png'
-import set from '../assets/settings.png'
-import fav from '../assets/star.png'
-import tre from '../assets/trend.png'
+import { AddIcon, SettingsIcon } from "@chakra-ui/icons";
+import { Button, ButtonGroup, Slide, useDisclosure } from "@chakra-ui/react";
+import home from "../assets/home.png";
+import ex from "../assets/explore.png";
+import set from "../assets/settings.png";
+import fav from "../assets/star.png";
+import tre from "../assets/trend.png";
+import { hover } from "@testing-library/user-event/dist/hover";
+import close from "../assets/close.png";
 const Sidebar = () => {
+  const { isOpen, onToggle } = useDisclosure()
   return (
+    // <Slide  direction='bottom' in={isOpen} style={{ zIndex: 10 }}>
     <Side>
-      <div>
-        <p style={{"fontSize":'30px', "fontWeight":"bolder", "textAlign":"center", "marginBottom":"20px"}} >Logo</p>
+      <div style={{"position":"relative", "display":"grid" , "gridColumn":'2'}}>
+        <p
+          style={{
+            fontSize: "30px",
+            fontWeight: "bolder",
+            textAlign: "center",
+            marginBottom: "20px",
+          }}
+        >
+          Logo
+        </p>
+          <img  style={{"position":"absolute" , "width":"10px" , "top":"10px" , "right":"2px" , 'marginTop':"3px" , "marginRight":'2px'}} src={close} alt="" />
       </div>
-      <div style={{"marginBottom":"20px"}}>
+      <div style={{ marginBottom: "20px" }}>
         {" "}
-        <Button  _hover='rgb(26,134,208)' width='3xs' variant="ghost" size='lg'>
-       <img src={home} width='25px' style={{"marginRight":"10px"}} /> Home
+        <Button className="button" width="3xs" variant="ghost" size="lg">
+          <img src={home} width="25px" style={{ marginRight: "10px" }} onClick={onToggle} /> Home
         </Button>
       </div>
-      <div style={{"marginBottom":"20px"}}>
+      <div style={{ marginBottom: "20px" }}>
         {" "}
-        <Button _hover='rgb(26,134,208)' width='3xs' variant="ghost" size='lg'>
-        <img src={tre} width='25px' style={{"marginRight":"10px"}} />  Trending
+        <Button className="button" width="3xs" variant="ghost" size="lg">
+          <img src={tre} width="25px" style={{ marginRight: "10px" }} />{" "}
+          Trending
         </Button>
       </div>
-      <div style={{"marginBottom":"20px"}}>
-        <Button  _hover='rgb(26,134,208)'  width='3xs' variant="ghost" size='lg'>
-        <img src={ex} width='25px' style={{"marginRight":"10px"}} /> Explore
+      <div style={{ marginBottom: "20px" }}>
+        <Button className="button" width="3xs" variant="ghost" size="lg">
+          <img src={ex} width="25px" style={{ marginRight: "10px" }} /> Explore
         </Button>
       </div>
-      <div style={{"marginBottom":"20px"}}>
-        <Button _hover='rgb(26,134,208)' width='3xs' variant="ghost" size='lg'>
-        <img src={fav} width='25px' style={{"marginRight":"10px"}} /> Favourites
+      <div style={{ marginBottom: "20px" }}>
+        <Button className="button" width="3xs" variant="ghost" size="lg">
+          <img src={fav} width="25px" style={{ marginRight: "10px" }} />{" "}
+          Favourites
         </Button>
       </div>
-      <div style={{"marginBottom":"20px"}}>
-        <Button  _hover='rgb(26,134,208)' width='3xs' variant="ghost" size='lg'>
-        <img src={set} width='25px' style={{"marginRight":"10px"}} />  Settings
+      <div style={{ marginBottom: "20px" }}>
+        <Button className="button" width="3xs" variant="ghost" size="lg">
+          <img src={set} width="25px" style={{ marginRight: "10px" }} />{" "}
+          Settings
         </Button>
       </div>
     </Side>
+ 
   );
 };
 
